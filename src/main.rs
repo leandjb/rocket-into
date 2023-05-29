87a82cfe63037b5da1bccb8a141d4224f00f9fe5
leandjb <leandjb@gmail.com>
2023-05-28 20:27:58 -0500

    add HTML Templates using handlebars

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-#[macro_use] extern crate rocket;
+#[macro_use]
+extern crate rocket;
+use rocket_dyn_templates::{Template, context};
 
 #[get("/")]
-fn index() -> &'static str { "HOME PAGE" }
+fn index() -> Template {
+    Template::render("index", context! {
+        title: "RESPONSE from Rocket.rs"
+    })
+}
 
 #[get("/about")]
 fn about() -> &'static str {
@@ -32,4 +38,5 @@ fn delete_profile() -> &'static str {
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index, about])
         .mount("/profile", routes![profile, create_profile, update_profile, delete_profile])
+        .attach(Template::fairing())
 }
