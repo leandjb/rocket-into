e9386add163d513759cc87a6b8709a7824b0134b
leandjb <leandjb@gmail.com>
2023-05-28 19:46:01 -0500

    add POST, PUT and PATCH methods

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,34 @@
 #[macro_use] extern crate rocket;
 
 #[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+fn index() -> &'static str { "HOME PAGE" }
+
+#[get("/about")]
+fn about() -> &'static str {
+    "ABOUT PAGE"
+}
+
+#[get("/profile")]
+fn create_profile() -> &'static str {
+    "PROFILE PAGE"
+}
+
+#[post("/profile")]
+fn profile() -> &'static str {
+    "Creating profile..."
+}
+
+#[patch("/profile")]
+fn update_profile() -> &'static str {
+    "profile updated"
+}
+
+#[delete("/profile")]
+fn delete_profile() -> &'static str {
+    "Deleting profile..."
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    rocket::build().mount("/", routes![index, about, profile, create_profile, update_profile, delete_profile])
 }
