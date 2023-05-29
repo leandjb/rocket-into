3b80feb906ef04f367d829a36799a226242eb8df
leandjb <leandjb@gmail.com>
2023-05-28 19:55:55 -0500

    Refactor on mount /profile site

--- src/main.rs
+++ src/main.rs
@@ -8,27 +8,28 @@ fn about() -> &'static str {
     "ABOUT PAGE"
 }
 
-#[get("/profile")]
+#[get("/")]
 fn create_profile() -> &'static str {
     "PROFILE PAGE"
 }
 
-#[post("/profile")]
+#[post("/")]
 fn profile() -> &'static str {
     "Creating profile..."
 }
 
-#[patch("/profile")]
+#[patch("/")]
 fn update_profile() -> &'static str {
     "profile updated"
 }
 
-#[delete("/profile")]
+#[delete("/")]
 fn delete_profile() -> &'static str {
     "Deleting profile..."
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index, about, profile, create_profile, update_profile, delete_profile])
+    rocket::build().mount("/", routes![index, about])
+        .mount("/profile", routes![profile, create_profile, update_profile, delete_profile])
 }
